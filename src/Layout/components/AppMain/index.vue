ecae7e5639ec890115562ec75b801700bc6cc573
坨坨坨坨 <2200630561@qq.com>
2024-11-01 19:31:45 +0800

    241101

---
+++ src/Layout/components/AppMain/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>我是appmain里面的内容</div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/Layout/components/TagsView/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>TagsView内容</div>
+</template>
+
+<style lang="scss" scoped></style>
